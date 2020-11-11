d4032c98ab507e7ef0608421b1bd84b2442403c5
Adam <adas.machaj@gmail.com>
2020-11-11 16:26:12 +0100

    Post Model

---
+++ Readly/Models/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Readly.Models
+{
+    public class PostDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Post Date")]
+        [DataType(DataType.Date)]
+        public DateTime PostDate { get; set; }
+    }
+}
