1a8ca911f82b07e280d4767651d810e349726cc0
Adam <adas.machaj@gmail.com>
2020-11-21 17:49:15 +0100

    Created post editor view

---
+++ Readly/ClientApp/src/components/PostEditor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class PostEditor extends Component {
+    render() {
+        return (
+          <div>
+            <h1>Post Editor</h1>
+          </div>
+        );
+      }
+}
