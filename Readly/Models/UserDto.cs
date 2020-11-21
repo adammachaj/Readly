d07036cb57504f16fca700d4411d16dd25c25a45
Adam <adas.machaj@gmail.com>
2020-11-21 14:04:01 +0100

    Created UserDto

---
+++ Readly/Models/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Readly.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Handle { get; set; }
+        public List<PostDto> Posts { get; set; }
+}
+}
