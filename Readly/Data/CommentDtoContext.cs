a001ed808a5bbac08be51891c39012234a14731b
Adam <adas.machaj@gmail.com>
2020-11-21 15:28:38 +0100

    Created CommentDto

---
+++ Readly/Data/CommentDtoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Readly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Readly.Data
+{
+    public class CommentDtoContext : DbContext
+    {
+        public CommentDtoContext(DbContextOptions<CommentDtoContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<CommentDto> Comment { get; set; }
+    }
+}
