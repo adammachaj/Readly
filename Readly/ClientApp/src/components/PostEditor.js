488d9ad58c83dbb6801af399efdfdf46241ec5f1
Adam <adas.machaj@gmail.com>
2020-11-22 22:31:23 +0100

    Simple editor

--- Readly/ClientApp/src/components/PostEditor.js
+++ Readly/ClientApp/src/components/PostEditor.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import 'draft-js/dist/Draft.css';
+import EditorJs from 'react-editor-js';
 
 export class PostEditor extends Component {
-    render() {
-        return (
-          <div>
+  render() {
+    return (
+        <div>
             <h1>Post Editor</h1>
-          </div>
-        );
-      }
+            <EditorJs/>
+        </div>
+        
+
+    );
+  }
 }
