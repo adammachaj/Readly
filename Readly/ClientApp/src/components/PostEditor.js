27319bf78bbaa837e998cdb742aa3e86b32615b1
Adam <adas.machaj@gmail.com>
2020-11-24 18:25:37 +0100

    Simple post editor

--- Readly/ClientApp/src/components/PostEditor.js
+++ Readly/ClientApp/src/components/PostEditor.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import 'draft-js/dist/Draft.css';
-import EditorJs from 'react-editor-js';
+import EditorJs from "../components/editor"
 
 export class PostEditor extends Component {
   render() {
     return (
         <div>
             <h1>Post Editor</h1>
-            <EditorJs/>
+            <div id="editorjs"></div>
+            <button class="btn">Save Article</button>
         </div>
-        
-
     );
   }
 }
---
+++ Readly/ClientApp/src/components/editor.js
@@ -0,0 +1,7 @@
+import EditorJs from '@editorjs/editorjs'; 
+// import Header from '@editorjs/header'; 
+// import List from '@editorjs/list'; 
+
+const editor = new EditorJs({ 
+  holder: 'editorjs'
+})
