ca549557c4077251b54be5eaf0008a2f75a304b2
Adam <adas.machaj@gmail.com>
2020-11-24 20:08:27 +0100

    Konstruktor

--- Readly/ClientApp/src/components/PostEditor.js
+++ Readly/ClientApp/src/components/PostEditor.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import 'draft-js/dist/Draft.css';
-import EditorJs from "../components/editor"
+import EditorJs from '@editorjs/editorjs'; 
 
 export class PostEditor extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.editor = new EditorJs({ 
+      holder: 'editorjs'
+    })
+
+    this.debug = this.debug.bind(this)
+  }
+
+  debug() {
+    // let saveBtn = document.getElementsByClassName('btn');
+    // console.log("asd");
+    // console.log(saveBtn);
+    this.editor.save().then((outputData) => {
+      console.log('Article data: ', outputData)
+    }).catch((error) => {
+      console.log('Saving failed: ', error)
+    });
+  }
+
   render() {
     return (
         <div>
             <h1>Post Editor</h1>
             <div id="editorjs"></div>
-            <button class="btn">Save Article</button>
+            <button class="btn" onClick={this.debug}>Save Article</button>
         </div>
     );
   }
