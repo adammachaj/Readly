92cdc41f1ff968b49e8b4b77f18f0075b43d56fb
Adam <adas.machaj@gmail.com>
2020-12-29 22:36:50 +0100

    Widok wyszukiwania

--- Readly/ClientApp/src/components/SearchTool.js
+++ Readly/ClientApp/src/components/SearchTool.js
@@ -5,7 +5,7 @@ export class SearchTool extends Component {
     handleSubmit = (event) => {
         event.preventDefault();
 
-        console.log()
+        console.log(document.getElementById("form3Example1").value)
 
         // const output = document.createElement('div');
         // const data = [...event.target.elements].reduce((data, element) => {
@@ -20,18 +20,29 @@ export class SearchTool extends Component {
 
     render() {
         return (
+
+            
             <div>
                 <h1>Search Tool</h1>
+
+                <form>
+    <div class="col">
+      <div class="form-outline">
+        <input name="Search" type="text" id="form3Example1" class="form-control" />
+        <label class="form-label" for="form3Example1">First name</label>
+      </div>
+    </div>
+
+  <button type="submit" class="btn btn-primary btn-block mb-4" onClick={this.handleSubmit}>Sign up</button>
+</form>
+
                 {/* <div id="editorjs"></div> */}
 
-                <form action="/search" onSubmit={this.handleSubmit}>
-                    <input type="text" name="q" />
+                {/* <form action="/search" onSubmit={this.handleSubmit}>
+                    <input id="bar" type="text" name="q" />
                     <button class="btn" onClick={this.handleSubmit}>Search</button>
-                </form>
+                </form> */}
             </div>
         );
     }
 }
-
-ReactDOM.render(<SearchForm />, document.getElementById('app'));
-  
