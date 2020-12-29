87ddb8f54a0ebec008e172e616b20b7120accdc8
Adam <adas.machaj@gmail.com>
2020-12-30 00:38:17 +0100

    Refactor

--- Readly/ClientApp/src/components/SearchTool.js
+++ Readly/ClientApp/src/components/SearchTool.js
@@ -6,42 +6,25 @@ export class SearchTool extends Component {
         event.preventDefault();
 
         console.log(document.getElementById("form3Example1").value)
-
-        // const output = document.createElement('div');
-        // const data = [...event.target.elements].reduce((data, element) => {
-        //   if (element.name && element.value) {
-        //     data[element.name] = element.value;
-        //   }
-        //   return data;
-        // }, {});
-        // output.textContent = JSON.stringify(data);
-        // document.body.appendChild(output);
     };
+
+    static renderArticles(articles) {
+        return 
+    }
 
     render() {
         return (
-
-            
             <div>
                 <h1>Search Tool</h1>
-
                 <form>
-    <div class="col">
-      <div class="form-outline">
-        <input name="Search" type="text" id="form3Example1" class="form-control" />
-        <label class="form-label" for="form3Example1">First name</label>
-      </div>
-    </div>
-
-  <button type="submit" class="btn btn-primary btn-block mb-4" onClick={this.handleSubmit}>Sign up</button>
-</form>
-
-                {/* <div id="editorjs"></div> */}
-
-                {/* <form action="/search" onSubmit={this.handleSubmit}>
-                    <input id="bar" type="text" name="q" />
-                    <button class="btn" onClick={this.handleSubmit}>Search</button>
-                </form> */}
+                    <div class="col">
+                    <div class="form-outline">
+                    <input name="Search" type="text" id="form3Example1" class="form-control" />
+                    <label class="form-label" for="form3Example1">First name</label>
+                    </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-block mb-4" onClick={this.handleSubmit}>Sign up</button>
+                </form>
             </div>
         );
     }
